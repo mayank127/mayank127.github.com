c4655a91110b6ee191ad672a967f2be8c2549d65
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2012-08-11 15:00:36 +0530

    updated

--- main.js
+++ main.js
@@ -64,6 +64,26 @@ function social_icon_move(){
 		document.querySelector("#to-top").style.top =  "-50px";
 	}
 }
+function smoothscroll(id){
+	var startY = document.body.scrollTop;
+	var stopY = document.querySelector("#"+id).offsetTop;
+	var distance = stopY > startY ? stopY - startY : startY - stopY;
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+}
 $(document).ready(function(){
 	$("#myform").validate({
 		debug: false,
